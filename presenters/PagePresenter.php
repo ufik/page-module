a256b70737af9e5718489fb8b424735e5b5da0c7
t.voslar@gmail.com <t.voslar@gmail.com>
2013-09-19 16:35:47 +0200

    initial commit

---
+++ presenters/PagePresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WebCMS\AdminModule;
+
+/**
+ * Description of PagePresenter
+ *
+ * @author Tomáš Voslař <tomas.voslar at webcook.cz>
+ */
+class PagePresenter extends \AdminModule\BasePresenter {
+	
+}
